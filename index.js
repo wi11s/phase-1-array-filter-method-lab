053835a7210f8b4d178a7a5b807f7cd7eeaef97c
William <wms274@nyu.edu>
2022-09-05 13:51:30 -0400

    commit tree d8383020d4e39ab97f3cd79e98d191ee0db7bf79

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(array, string) {
+    return array.filter(name => {
+        let lowerCaseName = name.toLowerCase();
+        let lowerCaseString = string.toLowerCase();
+        return lowerCaseName === lowerCaseString;
+    });
+};
+
+function fuzzyMatch(array, string) {
+    let inputLength = string.length;
+    return array.filter(name => {
+        return name.slice(0, inputLength) === string;
+    });
+};
+function matchName(array, string) {
+    return array.filter(element => {
+        return element.name === string;
+    });
+};
